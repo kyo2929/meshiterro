229a6d47261cd80be980080a382a2b54cc26ce51
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-06-15 06:32:09 +0000

    [Finish]4-18

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,14 +3,15 @@ class PostCommentsController < ApplicationController
   def create
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
+    comment.post_image_id = post_image.id
     comment.save
-    redirect_to post_image_path
+    redirect_to post_images_path
   end
   
   def destroy
     PostComment.find(params[:id]).destroy
     redirect_to post_image_path(params[:post_image_id])
-    
+  end
   
   private
   
