a4e238f056533e695dc1b71277e9fa18a2704f59
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-06-17 04:32:49 +0000

    [Finish]4-24

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,7 +7,7 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     if @post_image.save
-      redirect_to post_image_path
+      redirect_to post_images_path
     else
       render :new
     end
@@ -15,7 +15,7 @@ class PostImagesController < ApplicationController
   
 
   def index
-    @post_image = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
